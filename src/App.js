ca651bd206c3e0a3e7fb62e9e05546c0db6bdb58
Shubham Muthreja <muthrejashubham87@gmail.com>
2022-09-24 09:50:04 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ export default function App() {
       backgroundColor: "#9e9e9e",
       height: "100vh",
       width: "100vw"
+    },
+    headingBox :{
+         textAlign:"center",
+         display:"block"
+    },
+    mainHeading:{
+        color:"#fff",
+       fontSize :"40Px"
     }
   };
   return (
     <Box sx={styles.mainContainer}>
-      <Text>Welcome to ATM Simulator</Text>
+        <Box sx={styles.headingBox}>
+      <Text sx={styles.mainHeading}>Welcome to ATM Simulator</Text>
+      </Box>
       <Box></Box>
     </Box>
   );
